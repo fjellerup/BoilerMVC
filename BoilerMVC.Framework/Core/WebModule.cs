cc6bc466ce3f3abc4290760887d7d981c29fa25f
Mikkel Fjellerup Jensen <Mikkel@Codeweaver.dk>
2013-02-16 14:42:27 +0100

    removed some unwanted files

--- BoilerMVC.Framework/Core/WebModule.cs
+++ BoilerMVC.Framework/Core/WebModule.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using BoilerMVC.Common;
+﻿using BoilerMVC.Common;
 using BoilerMVC.Data;
-using Ninject;
 using Ninject.Modules;
 using Ninject.Web.Common;
 
@@ -15,9 +9,6 @@ namespace BoilerMVC.Framework
     {
         public override void Load()
         {
-            //IUnitOfWork shopContext = new BoilerMVCEntities();
-
-
             Kernel.Bind(typeof (IUnitOfWork)).To<BoilerMVCEntities>().InRequestScope();
             Kernel.Bind(typeof(IRepository<>)).To(typeof(EntityRepository<>));
         }
